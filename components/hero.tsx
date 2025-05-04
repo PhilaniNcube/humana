2623651e89b909f86a977d218f79e40073b83d2b
Philani Ncube <ncbphi001@gmail.com>
2025-05-04 09:42:39 +0200

    feat: wrap Introduction button in Link component for navigation

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 import { ChevronDown } from 'lucide-react'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
@@ -15,9 +16,17 @@ const Hero = () => {
           Putting youth at the centre of the TVET revolution across the continent
         </p>
         <div className="flex items-center justify-center mt-10">
-          <Button className='bg-brand-blue text-white text-2xl px-20 py-2.5 font-light rounded-sm'>Introduction</Button>
+          <Link href="/#initiative">
+          <Button className='bg-brand-blue hover:bg-blue-800 text-white text-2xl px-20 py-6 font-light rounded-sm'>Introduction</Button>
+          </Link>
         </div>
-        <div className="overflow-hidden">
+        <div className=" relative isolate">
+             <svg className='absolute -top-24 left-40 z-[-1] rotate-45' id='triangles' width="451" height="400" viewBox="0 0 451 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M152.5 253.499L0 310.999L183.5 366.499L152.5 253.499Z" fill="#D9D9D9" />
+            <path d="M434.5 144.999L162.5 217.499L349 302.499L434.5 144.999Z" fill="#F38E22" />
+            <path d="M364.715 140.308L450.172 0.683103L308.449 38.1447L364.715 140.308Z" fill="#00651B" />
+            <path d="M231 399.499L221 315.499L278.5 363.499L231 399.499Z" fill="#00B6CC" />
+          </svg>
           <Image
             src="/images/humana-hero.png"
             alt="hero"
